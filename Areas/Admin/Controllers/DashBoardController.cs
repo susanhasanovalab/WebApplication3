a1a796734934f4dad56d952aee178b00c72cb747
cff <cff@TITAN05>
2026-06-09 11:47:55 +0400

    Add project files.

---
+++ Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Areas.Admin.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        [Area("Admin")]
+             public IActionResult Index() {
+            return View();
+        }
+    } }
